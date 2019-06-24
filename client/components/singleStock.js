95f994300f7c941441816c0ff25c762fc0f00670
gwu358 <gwu358@gmail.com>
2019-06-23 20:27:20 -0400

    add functions: increase/decrease/edit stock quantity

--- client/components/singleStock.js
+++ client/components/singleStock.js
@@ -3,14 +3,36 @@ import {connect} from 'react-redux'
 import {fetchSingleStock} from '../store'
 
 class SingleStock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      quantity: 0
+    }
+    // this.editQuantity = this.editQuantity.bind(this);
+  }
+
+  IncrementItem = () => {
+    this.setState({quantity: this.state.quantity + 1})
+  }
+
+  DecreaseItem = () => {
+    this.setState({quantity: this.state.quantity - 1})
+  }
+
+  editQuantity = event => {
+    let quantity = Number(
+      event.target.validity.valid ? event.target.value : this.state.quantity
+    )
+    this.setState({quantity})
+  }
+
   componentDidMount() {
     const symbol = this.props.match.params.symbol
     this.props.fetchSingleStock(symbol)
   }
 
   render() {
-    const {stock} = this.props
-    console.log(stock, stock.open)
+    const {stock, buy} = this.props
     return (
       <div>
         open: {stock.open} {'\t'}
@@ -19,7 +41,18 @@ class SingleStock extends React.Component {
         close: {stock.close + '\t'}
         <br />
         current: {stock.current + '\t'}
-        <button onClick={() => {}}>Buy</button>
+        <br />
+        <input
+          type="text"
+          pattern="[0-9]*"
+          value={this.state.quantity}
+          onChange={this.editQuantity}
+        />
+        <button onClick={this.IncrementItem}>↑</button>
+        <button disabled={!this.state.quantity} onClick={this.DecreaseItem}>
+          ↓
+        </button>
+        <button onClick={() => buy(this.state.quantity)}>Buy</button>
       </div>
     )
   }
@@ -28,9 +61,13 @@ const mapState = state => ({
   stock: state.stock.single
 })
 
-const mapDispatch = dispatch => ({
+const mapDispatch = (dispatch, props) => ({
   fetchSingleStock: symbol => {
     dispatch(fetchSingleStock(symbol))
+  },
+  buy: quantity => {
+    const symbol = props.match.params.symbol.toUpperCase()
+    console.log(symbol, quantity)
   }
 })
 
